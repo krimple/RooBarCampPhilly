571ebf5cb1d857bb4317510d0d9eed76f1ad44ab
Ken Rimple <krimple@chariotsolutions.com>
2011-10-22 16:04:04 -0400

    now working

--- demos/events/src/main/java/org/barcamp/events/db/ClassSessionRepository.java
+++ demos/events/src/main/java/org/barcamp/events/db/ClassSessionRepository.java
@@ -3,6 +3,7 @@ package org.barcamp.events.db;
 import org.barcamp.events.models.ClassSession;
 import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
 
+
 @RooJpaRepository(domainType = ClassSession.class)
 public interface ClassSessionRepository {
 }
